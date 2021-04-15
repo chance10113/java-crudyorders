b8530f48162bf2d2b3426dfe973b904c5b49f500
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-04-14 21:56:00 -0500

    Finally MVP

--- src/main/java/lambda/school/javaordersproject/repositories/CustomerRepository.java
+++ src/main/java/lambda/school/javaordersproject/repositories/CustomerRepository.java
@@ -1,6 +1,8 @@
 package lambda.school.javaordersproject.repositories;
 
 import lambda.school.javaordersproject.models.Customer;
+import lambda.school.javaordersproject.views.AdvanceAmounts;
+import lambda.school.javaordersproject.views.OrderCounts;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
--- src/main/java/lambda/school/javaordersproject/services/PaymentServices.java
+++ src/main/java/lambda/school/javaordersproject/services/PaymentServices.java
@@ -1,7 +1,23 @@
 package lambda.school.javaordersproject.services;
 
 import lambda.school.javaordersproject.models.Payment;
+import lambda.school.javaordersproject.repositories.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
+@Transactional
+@Service(value = "paymentServices")
 public interface PaymentServices {
-    Payment save(Payment payment);
+
+    @Autowired
+    private PaymentRepository paymentrepos;
+
+    @Transactional
+    @Override
+    public Payment save(Payment payment)
+    {
+        return paymentrepos.save(payment);
+    }
 }
